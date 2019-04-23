493fce871fba592524cbcc9c53a9191cc95bbc7d
David Ortinau <david.ortinau@microsoft.com>
2019-04-23 14:57:51 -0500

    first files are the best files

---
+++ Straxa/Straxa/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using Straxa.Models;
+using Straxa.ViewModels;
+
+namespace Straxa.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        ItemDetailViewModel viewModel;
+
+        public ItemDetailPage(ItemDetailViewModel viewModel)
+        {
+            InitializeComponent();
+
+            BindingContext = this.viewModel = viewModel;
+        }
+
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+
+            var item = new Item
+            {
+                Text = "Item 1",
+                Description = "This is an item description."
+            };
+
+            viewModel = new ItemDetailViewModel(item);
+            BindingContext = viewModel;
+        }
+    }
+}
